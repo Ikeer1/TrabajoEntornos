cef0b48bb36242b9547dbb03cb3fbd8c31e24d75
ikerg <ikerg@DESKTOP-IAKLR9Q>
2019-05-03 14:28:23 +0200

    Primera Version

---
+++ Trabajo/src/TrabajoEnt.java
@@ -0,0 +1,17 @@
+
+public class TrabajoEnt {
+
+	int numero1 = 8, numero2 = 2;
+	int suma;
+	
+	void sumar() {
+		suma= (numero1 + numero2);
+		System.out.println("El resultado de la suma es: " + suma);
+	}
+	
+	public static void main(String[] args) {
+	TrabajoEnt tbj = new TrabajoEnt();
+	tbj.sumar();
+	}
+
+}
